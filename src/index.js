548251992420d60cb761d1c3a343f202e43ae350
sergey-krasnodar <serega.ru71@mail.ru>
2023-01-19 17:22:52 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBracket = ['(', '{', '[', '|', '1', '3', '5', '7', '8'];
+  const bracketPair = {
+    [')']: '(',
+    [']']: '[',
+    ['}']: '{',
+    ['|']: '|',
+    ['2']: '1',
+    ['4']: '3',
+    ['6']: '5',
+    ['7']: '7',
+    ['8']: '8'
+  }
+  let stack = []
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i]
+    if (openBracket.includes(currentSymbol) && currentSymbol !== stack[stack.length - 1]) {
+      stack.push(currentSymbol)      
+      } else {
+        if (stack.length === 0) {
+          return false;
+        } 
+          let topElement = stack[stack.length - 1]
+          if (topElement !== '|' || topElement !== '7' || topElement !== '8'){
+            stack.push(currentSymbol)
+          
+        }
+        if (bracketPair[currentSymbol] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+  }
+  return stack.length === 0;
 }
